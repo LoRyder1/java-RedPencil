ca9305a22a13684e3f9af3c37d2b3d623ca7b2cd
Lovinder Pnag <lspnag@gmail.com>
2016-03-09 14:24:52 -0500

    promo invalid if promo is not inbetween 5 and 30

---
+++ src/RedPencil.java
@@ -0,0 +1,15 @@
+
+public class RedPencil {
+
+    private int price;
+    private int promoPrice;
+
+    public RedPencil(int price, int promoPrice) {
+        this.price = price;
+        this.promoPrice = promoPrice;
+    }
+
+    public boolean valid() {
+        return true;
+    }
+}
--- test/RedPencilTest.java
+++ test/RedPencilTest.java
@@ -1,4 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class RedPencilTest {
 
+    @Test
+    public void promoPriceBetween5and30Percent() {
+        RedPencil promo = new RedPencil(100, 90);
+        assertEquals(true, promo.valid());
+    }
+
+    @Test
+    public void promoIsInvalidIfNotBetween5And30Percent() {
+        RedPencil promo = new RedPencil(100, 96);
+        assertEquals(false, promo.valid());
+    }
 }
