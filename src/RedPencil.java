6c2228e5d444d47a097e2bb9b0024defc57e0bc4
Lovinder Pnag <lspnag@gmail.com>
2016-03-09 21:31:59 -0500

    stablity at least 30 dyas

--- src/RedPencil.java
+++ src/RedPencil.java
@@ -1,15 +1,31 @@
 
 public class RedPencil {
 
-    private int price;
-    private int promoPrice;
+    private float price;
+    private float promoPrice;
+    private int stability;
 
-    public RedPencil(int price, int promoPrice) {
+    public RedPencil(float price, float promoPrice, int stability) {
         this.price = price;
         this.promoPrice = promoPrice;
+        this.stability = stability;
     }
 
     public boolean valid() {
-        return true;
+        float priceDiff = price - promoPrice;
+        float percentDiff = ((priceDiff / price) * 100);
+        if (percentDiff < 30 && percentDiff > 5 && stability >= 30) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean stable(int days) {
+        if (days >= 30) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- test/RedPencilTest.java
+++ test/RedPencilTest.java
@@ -4,15 +4,27 @@ import static junit.framework.TestCase.assertEquals;
 
 public class RedPencilTest {
 
+    private RedPencil promo;
+
+    public void setPromo(int price, int promoPrice, int stability) {
+        promo = new RedPencil(price, promoPrice, stability);
+    }
+
     @Test
     public void promoPriceBetween5and30Percent() {
-        RedPencil promo = new RedPencil(100, 90);
+        setPromo(100, 90, 30);
         assertEquals(true, promo.valid());
     }
 
     @Test
     public void promoIsInvalidIfNotBetween5And30Percent() {
-        RedPencil promo = new RedPencil(100, 96);
+        setPromo(100, 96, 30);
+        assertEquals(false, promo.valid());
+    }
+
+    @Test
+    public void previousPriceMustBeStableForAtLeast30Days() {
+        setPromo(100, 90, 29);
         assertEquals(false, promo.valid());
     }
 }
